d42e48619c7c52888b0610be7ae0342b36ed31eb
Roland Bapst <roland.bapst@tamedia.ch>
2017-11-30 17:32:19 +0100

    First CI

---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?php
+echo '<html><head>';
+echo '<title>';
+echo gethostname();
+echo '</title>';
+echo '</head><body>';
+echo '<h1>';
+echo gethostname();
+echo '</h1>';
+echo '</body></html>';
+?>
