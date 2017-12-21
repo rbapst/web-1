09374c69240ce82c11a0003c8c9d5b2ebde0683e
Roland Bapst <roland.bapst@tamedia.ch>
2017-12-21 18:33:59 +0100

    updated file to download

--- ec2start.sh
+++ ec2start.sh
@@ -1,3 +1,11 @@
-git clone https://github.com/rbapst-tamedia/web.git
-cp web/www/index.php /var/www/html
-cp web/www/.check.php /var/www/html
+#!/bin/bash
+yum install -y httpd php
+wget https://github.com/rbapst-tamedia/web/archive/master.zip
+unzip master.zip
+cp web-master/www/index.php /var/www/html
+cp web-master/www/.check.html /var/www/html
+chkconfig --add httpd
+chkconfig httpd on
+service httpd start
+rm -f master.zip
+rm -rf web-master
